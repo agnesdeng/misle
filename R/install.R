83b27bb53b915c73b81ec5a8531e5b2b5d8f534c
(Agnes) Yongshi Deng <25675889+agnesdeng@users.noreply.github.com>
2019-11-29 01:27:47 +1300

    Update install.R

--- R/install.R
+++ R/install.R
@@ -1,63 +1,8 @@
 #' Install the TensorFlow backend (modified code from Keras)
 #'
 #' TensorFlow will be installed in to an "r-tensorflow" virtual or conda environment.
-#' Note: "virtualenv" is not avilable on Windows
-#'
+#' Note: "virtualenv" is not avilable on Windows#'
 #' @inheritParams tensorflow::install_tensorflow
-#'
-#' @param method Installation method ("virtualenv" or "conda")
-#'
-#' @param conda_python_version the python version installed in the created conda
-#'   environment. Python 3.6 is installed by default.
-#'
-#' @param tensorflow TensorFlow version to install. Specify "default" to install
-#'   the CPU version of the latest release. Specify "gpu" to install the GPU
-#'   version of the latest release.
-#'
-#' @param extra_packages Additional PyPI packages to install along with
-#'   Keras and TensorFlow.
-#'
-#' @param ... Other arguments passed to [tensorflow::install_tensorflow()].
-#'
-#' @section GPU Installation:
-#'
-#' TensorFlow can be configured to run on either CPUs or GPUs. The CPU
-#' version is much easier to install and configure so is the best starting place
-#' especially when you are first learning how to use misle.
-#'
-#' - *TensorFlow with CPU support only*. If your system does not have a NVIDIA® GPU,
-#' you must install this version. Note that this version of TensorFlow is typically
-#' much easier to install, so even if you have an NVIDIA GPU, we recommend installing
-#' this version first.
-#'
-#' - *TensorFlow with GPU support*. TensorFlow programs typically run significantly
-#' faster on a GPU than on a CPU. Therefore, if your system has a NVIDIA® GPU meeting
-#' all prerequisites and you need to run performance-critical applications, you should
-#' ultimately install this version.
-#'
-#' To install the GPU version:
-#'
-#' 1) Ensure that you have met all installation prerequisites including installation
-#'    of the CUDA and cuDNN libraries as described in [TensorFlow GPU Prerequistes](https://tensorflow.rstudio.com/installation_gpu.html#prerequisites).
-#'
-#' 2) Pass `tensorflow = "gpu"` to `install_misle()`. For example:
-#'
-#'     ```
-#'       install_misle(tensorflow = "gpu")
-#'     ````
-#'
-#' @section Windows Installation:
-#'
-#' The only supported installation method on Windows is "conda". This means that you
-#' should install Anaconda 3.x for Windows prior to installing Keras.
-#'
-#'
-#' @section Additional Packages:
-#'
-#' If you wish to add additional PyPI packages to your Keras / TensorFlow environment you
-#' can either specify the packages in the `extra_packages` argument of `install_misle()`,
-#' or alternatively install them into an existing environment using the
-#' [reticulate::py_install()] function.
 #'
 #' @examples
 #' \dontrun{
